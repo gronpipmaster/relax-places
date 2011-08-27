2afa650255b97879430873857b19568cd8f1f83f
Gronpipmaster <Gronpipmaster@gmail.com>
2011-08-27 19:55:32 +0700

    editing content view

--- relax/protected/views/site/index.php
+++ relax/protected/views/site/index.php
@@ -1,26 +1,20 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 <h1><?php echo CHtml::encode(Yii::app()->name); ?></h1>
-			<form class="form search" action="">
-				<input tipe="text" name="who" />
-				<input tipe="text" name="radius" />
-				<input type="submit" value="search" />
-			</form>
+	<form class="form search" action="">
+			<input tipe="text" name="who" />
+			<input tipe="text" name="radius" />
+			<input type="submit" value="search" />
+	</form>
 	<div class="map" id="map_canvas"></div>
 	<div class="sidebar">
-				<div class="block">
-					<h2>Timeline</h2>
-					<div class="block-inner">
-						<ul>
-							<li></li>
-						</ul>
-					</div>
-				</div>
-				<div class="block">
-					<h2>Self</h2>
-					<div class="block-inner">
-						<ul>
-							<li></li>
-						</ul>
-					</div>
-	</div>
+		<div class="block">
+			<h2>Timeline</h2>
+			<div class="block-inner">
+			</div>
+		</div>
+		<div class="block">
+			<h2>Self</h2>
+			<div class="block-inner">
+			</div>
+		</div>
 </div>
