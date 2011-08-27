52d438fe0282016aac1f24c6f8f80ebd08b0f0bf
Gronpipmaster <Gronpipmaster@gmail.com>
2011-08-27 19:44:59 +0700

    update view

--- relax/protected/views/site/index.php
+++ relax/protected/views/site/index.php
@@ -1,16 +1,26 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
-
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <tt><?php echo __FILE__; ?></tt></li>
-	<li>Layout file: <tt><?php echo $this->getLayoutFile('main'); ?></tt></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<h1><?php echo CHtml::encode(Yii::app()->name); ?></h1>
+			<form class="form search" action="">
+				<input tipe="text" name="who" />
+				<input tipe="text" name="radius" />
+				<input type="submit" value="search" />
+			</form>
+	<div class="map" id="map_canvas"></div>
+	<div class="sidebar">
+				<div class="block">
+					<h2>Timeline</h2>
+					<div class="block-inner">
+						<ul>
+							<li></li>
+						</ul>
+					</div>
+				</div>
+				<div class="block">
+					<h2>Self</h2>
+					<div class="block-inner">
+						<ul>
+							<li></li>
+						</ul>
+					</div>
+	</div>
+</div>
