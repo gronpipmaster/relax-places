968026dce7b3ade08e6d8222bc135db408832dc5
Gronpipmaster <Gronpipmaster@gmail.com>
2011-08-27 12:39:23 +0700

    Типо вью

---
+++ tmpl/index.php
@@ -0,0 +1,19 @@
+<?php
+require_once './config.php';
+?>
+<!DOCTYPE html >
+<html xml:lang="ru-RU" lang="ru-RU" >
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="<?=$source?>css/ui.css" />
+	<title><?=$title?></title>
+	<script type="text/javascript" src="http://yandex.st/jquery/1.6.1/jquery.min.js" charset="utf-8"></script>
+	<script type="text/javascript" src="<?=$source?>js/ui.js" charset="utf-8"></script>
+</head><body>
+	<div class="wrapper">
+		<div class="inner">
+			<h1><?=$title?></h1>
+			
+		</div>
+	</div>
+</body></html>
