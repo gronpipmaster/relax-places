5c5a85206897bbeb897f1c249383f11f1c891c9d
wDevil <wdevil@Vitaliy-Kulpins-MacBook-Air.local>
2011-08-27 13:49:27 +0700

    Added controlelr and changed cfg for db connection

--- relax/protected/config/main.php
+++ relax/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Relax places',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -47,19 +47,18 @@ return array(
 			),
 		),
 		*/
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+		//'db'=>array(
+	//		'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	//	),
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=172.16.23.218;dbname=relax_places',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'relax_places',
+			'password' => 'relax_places',
 			'charset' => 'utf8',
 		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
             'errorAction'=>'site/error',
