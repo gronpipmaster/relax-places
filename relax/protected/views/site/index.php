4047eb3161ce75aded2ab6f44d49cbe6e142877b
Gronpipmaster <Gronpipmaster@gmail.com>
2011-08-28 16:10:22 +0700

    update form action

--- relax/protected/views/site/index.php
+++ relax/protected/views/site/index.php
@@ -1,12 +1,19 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 <h1><?php echo CHtml::encode(Yii::app()->name); ?></h1>
+	
 	<form class="form search" action="<?php echo Yii::app()->request->baseUrl; ?>/index.php" id="search">
 			<input type="hidden" name="r" value="app/getGeom" />
 			<input type="text" name="what" id="what" />
-			<input type="text" name="radius" id="radius" />
 			<ul class="result">
 			</ul>
-			<input type="submit" value="search" />
+			
+	</form>
+	<form class="form searchPlace" action="<?php echo Yii::app()->request->baseUrl; ?>/index.php" id="searchNearBy">
+		<input type="hidden" name="r" value="app/searchNearBy" />
+		<input type="hidden" name="lat" value="" />
+		<input type="hidden" name="lon" value="" />
+		<input type="text" name="radius" id="radius" />
+		<input type="submit" value="search" />
 	</form>
 	<div class="map" id="map_canvas"></div>
 	<div class="sidebar">
